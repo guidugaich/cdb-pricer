62e355b96c7045e4a7eeb9951a21648b4851164d
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-09 20:54:45 -0300

    feat: middleware de validação de input

--- api/app.js
+++ api/app.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 
+const { inputValidation } = require('../middlewares/inputValidation');
+
+app.use(inputValidation)
+
 app.get('/pricing', (req, res) => {
   const { investmentDate, cdbRate, currentDate } = req.query;
 
