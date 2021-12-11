a9e6347a71ce490506c2b71337f1d3c09900bf6d
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-11 08:07:25 -0300

    feat: model para extração dos dados

---
+++ model/index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const csv = require('csvtojson');
+const fs = require('fs').promises
+
+const CDI_DATA_URL = 'https://gorila-blog.s3-us-west-2.amazonaws.com/CDI_Prices.csv'
+
+function formatDate(date) {
+  return `${date.split('/')[2]}-${date.split('/')[1]}-${date.split('/')[0]}`
+}
+
+async function getCDIHistory() {
+  const { data } = await axios.get(CDI_DATA_URL, { responseType: 'blob' });
+
+  const CDIHistoryArray = await csv().fromString(data);
+
+  const CDIHistoryArrayParsed = CDIHistoryArray.map((obj) => {
+    return ({ ...obj, dtDate: formatDate(obj.dtDate) })
+  })
+  
+  return CDIHistoryArrayParsed;
+}
+
+module.exports = {
+  getCDIHistory
+}
