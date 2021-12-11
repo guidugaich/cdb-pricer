c6e93cdc64bbb498f01301e90e4d15d04a0c334c
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-11 08:06:41 -0300

    fix: refatorando validação

--- api/app.js
+++ api/app.js
@@ -1,16 +1,18 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 
+const { getCDIHistory } = require('../model')
+
 app.use(bodyParser.json());
 
 const { inputValidation } = require('../middlewares/inputValidation');
 
 app.use(inputValidation)
 
-app.get('/pricing', (req, res) => {
-  const { investmentDate, cdbRate, currentDate } = req.query;
+app.get('/pricing', async (req, res) => {
+  const { investmentDate, cdbRate, currentDate } = req.inputs;
 
-  res.status(200).json({ investmentDate, cdbRate: parseFloat(cdbRate) , currentDate })
+  res.status(200).json({ investmentDate, cdbRate, currentDate })
 })
 
 module.exports = app;
