5e128c96c28658e0bd5ef8fe007fabc9822c5bb3
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-11 23:07:59 -0300

    feat: mocks para testes

---
+++ tests/mocks/mockServiceOutput.js
@@ -0,0 +1,120 @@
+module.exports = {
+  data: [
+    {
+      date: '2016-11-14',
+      unitPrice: 1000.53,
+    },
+    {
+      date: '2016-11-16',
+      unitPrice: 1001.07,
+    },
+    {
+      date: '2016-11-17',
+      unitPrice: 1001.6,
+    },
+    {
+      date: '2016-11-18',
+      unitPrice: 1002.14,
+    },
+    {
+      date: '2016-11-21',
+      unitPrice: 1002.67,
+    },
+    {
+      date: '2016-11-22',
+      unitPrice: 1003.21,
+    },
+    {
+      date: '2016-11-23',
+      unitPrice: 1003.74,
+    },
+    {
+      date: '2016-11-24',
+      unitPrice: 1004.28,
+    },
+    {
+      date: '2016-11-25',
+      unitPrice: 1004.82,
+    },
+    {
+      date: '2016-11-28',
+      unitPrice: 1005.35,
+    },
+    {
+      date: '2016-11-29',
+      unitPrice: 1005.89,
+    },
+    {
+      date: '2016-11-30',
+      unitPrice: 1006.43,
+    },
+    {
+      date: '2016-12-01',
+      unitPrice: 1006.95,
+    },
+    {
+      date: '2016-12-02',
+      unitPrice: 1007.48,
+    },
+    {
+      date: '2016-12-05',
+      unitPrice: 1008.01,
+    },
+    {
+      date: '2016-12-06',
+      unitPrice: 1008.54,
+    },
+    {
+      date: '2016-12-07',
+      unitPrice: 1009.07,
+    },
+    {
+      date: '2016-12-08',
+      unitPrice: 1009.6,
+    },
+    {
+      date: '2016-12-09',
+      unitPrice: 1010.13,
+    },
+    {
+      date: '2016-12-12',
+      unitPrice: 1010.66,
+    },
+    {
+      date: '2016-12-13',
+      unitPrice: 1011.19,
+    },
+    {
+      date: '2016-12-14',
+      unitPrice: 1011.72,
+    },
+    {
+      date: '2016-12-15',
+      unitPrice: 1012.25,
+    },
+    {
+      date: '2016-12-16',
+      unitPrice: 1012.78,
+    },
+    {
+      date: '2016-12-19',
+      unitPrice: 1013.32,
+    },
+    {
+      date: '2016-12-20',
+      unitPrice: 1013.85,
+    },
+    {
+      date: '2016-12-21',
+      unitPrice: 1014.38,
+    },
+    {
+      date: '2016-12-22',
+      unitPrice: 1014.91,
+    },
+    {
+      date: '2016-12-23',
+      unitPrice: 1015.45,
+    },
+  ],
+};
