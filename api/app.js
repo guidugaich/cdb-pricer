d0052b45c16132e3bbc104be8ff77793c9317b9e
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-12-12 09:37:00 -0300

    feat: allow CORS

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = require('express')();
 
 const { pricing } = require('../controller');
 const { inputValidation } = require('../middlewares/inputValidation');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(inputValidation);
 
 app.get('/pricing', pricing);
